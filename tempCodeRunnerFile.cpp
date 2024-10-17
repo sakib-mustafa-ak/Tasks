7b7382037f4565ddc41540b76ae9bf39968465e9
Sakib Mustafa <147903390+sakib-mustafa-ak@users.noreply.github.com>
2024-10-17 14:47:31 +0600

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,20 @@
-if (a[low] >= left)
-        //     return low;
-        // if (a[high] < left)
-        //     return -1;
-        // if (a[high] == left)
-        //     return high;
+int t;
+cin>>t;
+while(t--)
+t
+ll n.m;
+cin >>n>>m;
+ll x.y.k;
+cin >> x >> y>>K;
+ll x1=x-1.x2-n-x;
+ll y1=y-1.y2=m-y:
+if(k==0)
+cout <<0<nn;
+cout << max (max(x1.x2)*m.max (y1.y2)*n) << nn:
+else if(k==l)
+else if(k==2)
+cout << n*m-min(((min(x1.x2)+1)* (min(y1.y2)+1)).min(n,m)) << nn:
+else if(k==3)
+cout << n*m-1-min((x1.x2.y1.y2)) << nn;
+else
+cout << n*m-l.<<nn;
